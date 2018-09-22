bf99cb75de471703acc9a20beaf76bc2a42de14a
Svetosl <lenyuks@gmail.com>
2018-09-21 21:40:26 -0400

    Updated Version

--- Main Code/criterion.py
+++ Main Code/criterion.py
@@ -6,7 +6,7 @@ def Number(rawData)
         bicArray = []
         #aicArray = []
         n_components = np.arange(1, 20)
-        models = [GMM(n, covariance_type='full', random_state=0).fit(rowData) for n in n_components]
+        models = [GMM(n, covariance_type='full', random_state=0).fit(rawData) for n in n_components]
         for m in models    
             bicArray.append(m.bic(rawData))
             #aicArray.append(m.aic(rawData))
