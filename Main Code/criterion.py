0a8d7ec77ba580281b8cd7eebb282d7eecb47112
Svetosl <lenyuks@gmail.com>
2018-09-21 21:26:09 -0400

    This is the criterion

---
+++ Main Code/criterion.py
@@ -0,0 +1,14 @@
+import numpy as np
+from sklearn.mixture import GMM
+
+
+def Number(rawData)
+        bicArray = []
+        #aicArray = []
+        n_components = np.arange(1, 20)
+        models = [GMM(n, covariance_type='full', random_state=0).fit(rowData) for n in n_components]
+        for m in models    
+            bicArray.append(m.bic(rawData))
+            #aicArray.append(m.aic(rawData))
+        return min(bicArray)
+
