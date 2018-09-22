11cd97ce00ae82700f68d45ce18a41f2a80c538f
atkinssamuel <purpleandgold73@gmail.com>
2018-09-22 00:25:11 -0400

    Optimized parse.py

--- Main Code/parse.py
+++ Main Code/parse.py
@@ -1,40 +1,34 @@
 import csv
 import numpy as np
 
-def Normalize(DataPath):
-    with open(DataPath+'/claims_final.csv') as DataSetFile:
-        DataSet = csv.reader(DataSetFile, delimiter=',')
-        DataMatrix = []
-        for entry in DataSet:
-            DataMatrix.append(entry)
-        MaxMatrix = [0] * 8
-        MinMatrix = [0] * 8
-        for row in DataMatrix:
-            for i in range(0, 8):
-                if i != 7:
-                    if i != 4:
-                        row[i] = int(row[i])
-                    continue
-                if i == 7:
-                    row[i] = float(row[i])
-                if row[i] < MinMatrix[i]:
-                    MinMatrix[i] = row[i]
-                if row[i] > MaxMatrix[i]:
-                    MaxMatrix[i] = row[i]
-        for row in DataMatrix:
+def Normalize(dataPath):
+    with open(dataPath+'/claims_final.csv') as dataSetFile:
+        dataSet = csv.reader(dataSetFile, delimiter=',')
+        dataMatrix = []
+        for entry in dataSet:
+            dataMatrix.append(entry)
+        maxMatrix = [0] * 8
+        minMatrix = [0] * 8
+        for row in dataMatrix:
+            row[7] = float(row[7])
+            if row[7] < minMatrix[7]:
+                minMatrix[7] = row[7]
+            if row[7] > maxMatrix[7]:
+                maxMatrix[7] = row[7]
+        for row in dataMatrix:
             for i in range(0, 8):
                 if i != 7:
                     continue
-                row[i] = (row[i] - MinMatrix[i])/(MaxMatrix[i] - MinMatrix[i])
-    return DataMatrix
+                row[i] = (row[i] - minMatrix[i])/(maxMatrix[i] - minMatrix[i])
+    return dataMatrix
 
-def Extract(Data):
+def Extract(data):
     extData = []
-    for i in range(len(Data)):
+    for i in range(len(data)):
         extData.append([])
-        extData[i].append(Data[i][2])
-        extData[i].append(Data[i][6])
-        extData[i].append(Data[i][7])
+        extData[i].append(data[i][2])
+        extData[i].append(data[i][6])
+        extData[i].append(data[i][7])
     ext = np.r_[extData]
     print(ext)
     return ext
