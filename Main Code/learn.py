d06384b7c6ca35d141d8bcda57ff2747ce2fe3ca
agiachris <c.agia@yahoo.com>
2018-09-22 19:22:52 -0400

    Plot Provider vs Amount Claimed

--- Main Code/learn.py
+++ Main Code/learn.py
@@ -4,7 +4,7 @@ from sklearn.mixture import GaussianMixture
 def Learn(X, n, state):
     gmm = GaussianMixture(n_components=n, covariance_type='full', random_state=0).fit(X) 	# Create Gaussian Classifier
     labels = gmm.predict(X)																	# Predict Labels/Cluster
-    plt.scatter(X[:, 1], X[:, 4], c=labels, s=1, cmap='viridis');							# Plot
+    plt.scatter(X[:, 1], X[:, 5], c=labels, s=1, cmap='viridis');							# Plot
     plt.xlabel("Provider Type")																	
     plt.ylabel("Dollar Amount of Claim")
     plt.title(state)
