f3e52a396c058d3a8e0749850ac42bfbdeb698a2
agiachris <c.agia@yahoo.com>
2018-09-22 04:15:35 -0400

    Developing scoring system

--- Main Code/criterion.py
+++ Main Code/criterion.py
@@ -3,7 +3,7 @@ from sklearn.mixture import GaussianMixture
 
 def Number(rawData):
     bicArray = []
-    n_components = np.arange(15, 100)
+    n_components = np.arange(10, 50)
     for i in range(len(n_components)):
         model = GaussianMixture(n_components=i+1, covariance_type='full', random_state=0, warm_start=True).fit(rawData)
         bicArray.append(model.bic(rawData))
--- Main Code/main.py
+++ Main Code/main.py
@@ -1,17 +1,24 @@
 # Learning, Plotting and Other Files
+import numpy as np
 import criterion
 import parse 
 import learn
+import rank
 
 if __name__ == "__main__":
 
 	X = parse.Normalize('/home/agiachris/CIBCData')				# Return Dicitonary with States as Keys and databases and values
-
+	stored_data = []
 	for key in X.keys():										# iterate over datasets
 		print(key)
 		currData = X[key]
-		currData = currData[:100000]
+		stored_data += currData
+		currData = currData[:100]
 		extData = parse.Extract(currData)						# Extract required data (doctor type, price)
-		n = criterion.Number(extData)							# Figuring out the best posibble criterion
-		print(n)
-		learn.Learn(extData, 50)								# Cluster and plot
+		#n = criterion.Number(extData)							# Figuring out the best posibble criterion
+		if key == 'NY':
+			score_records = learn.Learn(extData, 50, key)									# Cluster, plot, and store
+		else:
+			score_records = np.r_[score_records, (learn.Learn(extData, 50, key))]			# Cluster, plot, and store
+
+	rank.Rank(score_records, stored_data)
